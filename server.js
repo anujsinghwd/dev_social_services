b9ffb3cf3cee4368be362c42f78bcb55b3d344bb
anujsinghwd <anuj_singh@outlook.in>
2022-08-02 21:49:00 +0530

    DEV APIs

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const users = require("./routes/users");
+const profile = require('./routes/profile');
+const posts = require('./routes/posts');
+const passport = require("passport");
+const app = express();
+
+//Body parser middleware
+app.use(express.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch((er) => console.log(err));
+
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
+app.use("/api/users", users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
